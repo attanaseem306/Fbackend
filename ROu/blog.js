7ddbc920ca5c4f52fb58a2bdc80804af9fa61cfe
MRMutahir <mutahirkareem820@example.com>
2023-11-14 18:57:39 +0500

    fbg

--- ROu/blog.js
+++ ROu/blog.js
@@ -3,7 +3,7 @@ const routes = app.Router()
 const blogs = require('../Model/blog')
 
 routes.get('/', async(req, res) => {
-    const userBlogpost=await users.find().populate('user').exec()
+    const userBlogpost=await blogs.find()
  res.status(200).send({
     status: 200,
     user: userBlogpost
@@ -13,7 +13,7 @@ routes.get('/', async(req, res) => {
 
 routes.post('/',async(req,res)=>{
   
- const userBlogpost=await users.create({...req.body})
+ const userBlogpost=await blogs.create({...req.body})
  res.status(200).send({
     status: 200,
     user: userBlogpost
@@ -24,58 +24,61 @@ routes.post('/',async(req,res)=>{
 
 
 
-routes.delete('/:id', (req, res) => {
+
+
+
+// routes.delete('/:id', (req, res) => {
   
-    users.splice(req.params.id-1,1)
+//     users.splice(req.params.id-1,1)
     
-    res.status(200).send({
-        status: 200,
-        user: users
+//     res.status(200).send({
+//         status: 200,
+//         user: users
 
-    })
-})
+//     })
+// })
 
 
-routes.put('/:id', (req, res) => {
+// routes.put('/:id', (req, res) => {
   
-if (users[req.params.id-1]) {
-  users[req.params.id-1].name='Change name'
-  res.status(200).send({
-    status: 200,
-    message: users
-})
+// if (users[req.params.id-1]) {
+//   users[req.params.id-1].name='Change name'
+//   res.status(200).send({
+//     status: 200,
+//     message: users
+// })
 
-}    
+// }    
 
-else{
-    res.status(200).send({
-        status:500,
-        message:'user not found'
-    })
-}
+// else{
+//     res.status(200).send({
+//         status:500,
+//         message:'user not found'
+//     })
+// }
     
-})
+// })
 
 
-routes.get('/:id', (req, res) => {
-   console.log('pharams0', req.params);
-    const singalUser = users.find((data) => data.id == req.params.id)
+// routes.get('/:id', (req, res) => {
+//    console.log('pharams0', req.params);
+//     const singalUser = users.find((data) => data.id == req.params.id)
 
-    if (!singalUser) {
-        res.status(500).send({
-            status: 500,
-            message: 'Error User is not found'
-        })
-    }
+//     if (!singalUser) {
+//         res.status(500).send({
+//             status: 500,
+//             message: 'Error User is not found'
+//         })
+//     }
 
-    if (singalUser) {
-        res.status(200).send({
-            status: 200,
-            singalUser
-        })
-    }
+//     if (singalUser) {
+//         res.status(200).send({
+//             status: 200,
+//             singalUser
+//         })
+//     }
 
 
-})
+// })
 
 module.exports = routes
