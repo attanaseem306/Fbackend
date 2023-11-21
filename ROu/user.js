ba86244afe8856f5d8e59a6cb151e1475d41f65e
Amrkhwazir <amrkh5678@gmail.com>
2023-11-21 18:21:46 +0500

    sshg

--- ROu/user.js
+++ ROu/user.js
@@ -2,43 +2,6 @@ const app = require('express')
 const users = require('../Model/user')
 const routes = app.Router()
 
-routes.get('/', async (req, res) => {
-    const user = await users.find()
-    res.status(200).send({
-        status: 200,
-        user
 
-    })
-})
-
-routes.post('/', async (req, res) => {
-    const user = await users.create({ ...req.body })
-    res.send({
-        status: 200,
-        user
-
-    })
-})
-
-
-
-routes.delete('/:id', (req, res) => {
-
-
-})
-
-
-routes.put('/:id', (req, res) => {
-
-
-
-})
-
-
-routes.get('/:id', (req, res) => {
-
-
-
-})
 
 module.exports = routes
